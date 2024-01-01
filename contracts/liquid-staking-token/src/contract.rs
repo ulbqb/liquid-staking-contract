a9f2d9ab59e8b631135024549a9cfcfa4bc94ca3
Shogo Hyodo <mmoshg8u@gmail.com>
2024-01-01 15:59:29 +0900

    Make mint unavailable

--- contracts/liquid-staking-token/src/contract.rs
+++ contracts/liquid-staking-token/src/contract.rs
@@ -293,48 +293,13 @@ pub fn execute_burn(
 }
 
 pub fn execute_mint(
-    deps: DepsMut,
+    _deps: DepsMut,
     _env: Env,
-    info: MessageInfo,
-    recipient: String,
-    amount: Uint128,
+    _info: MessageInfo,
+    _recipient: String,
+    _amount: Uint128,
 ) -> Result<Response, ContractError> {
-    let mut config = TOKEN_INFO
-        .may_load(deps.storage)?
-        .ok_or(ContractError::Unauthorized {})?;
-
-    if config
-        .mint
-        .as_ref()
-        .ok_or(ContractError::Unauthorized {})?
-        .minter
-        != info.sender
-    {
-        return Err(ContractError::Unauthorized {});
-    }
-
-    // update supply and enforce cap
-    config.total_supply += amount;
-    if let Some(limit) = config.get_cap() {
-        if config.total_supply > limit {
-            return Err(ContractError::CannotExceedCap {});
-        }
-    }
-    TOKEN_INFO.save(deps.storage, &config)?;
-
-    // add amount to recipient balance
-    let rcpt_addr = deps.api.addr_validate(&recipient)?;
-    BALANCES.update(
-        deps.storage,
-        &rcpt_addr,
-        |balance: Option<Uint128>| -> StdResult<_> { Ok(balance.unwrap_or_default() + amount) },
-    )?;
-
-    let res = Response::new()
-        .add_attribute("action", "mint")
-        .add_attribute("to", recipient)
-        .add_attribute("amount", amount);
-    Ok(res)
+    panic!("mint is not availabe");
 }
 
 pub fn execute_send(
